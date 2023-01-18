56eae74a0aaa6b2d0e4cd36a5e7260dba3c2068b
ahmed-m-abdelfatah <ahmedmo.engineer@gmail.com>
2023-01-18 22:00:26 +0200

    ui done navbar

--- client/src/components/Content/Content.js
+++ client/src/components/Content/Content.js
@@ -1,7 +1,12 @@
 import './Content.scss';
+import Navbar from '../navbar/Navbar.js';
 
 const Content = () => {
-  return <div className='content'>content</div>;
+  return (
+    <div className='content'>
+      <Navbar />
+    </div>
+  );
 };
 
 export default Content;
---
+++ client/src/components/navbar/Navbar.js
@@ -0,0 +1,39 @@
+import './Navbar.scss';
+import avatar from '../../img/avatar.png';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBars, faSearch, faBell, faEnvelope } from '@fortawesome/free-solid-svg-icons';
+
+const Navbar = () => {
+  return (
+    <nav className='nav'>
+      <div className='row'>
+        <button className='icon'>
+          <FontAwesomeIcon size='xl' icon={faBars} />
+        </button>
+
+        <h2 className='welcome'>welcome,talia</h2>
+      </div>
+
+      <div className='row'>
+        <div className='search'>
+          <FontAwesomeIcon className='search-icon' size='sm' icon={faSearch} />
+          <input type='search' name='search' placeholder='search...' autocomplete='off' />
+        </div>
+
+        <div className='icons'>
+          <span className='icon'>
+            <span className='number'>2</span>
+            <FontAwesomeIcon size='xl' icon={faBell} />
+          </span>
+          <span className='icon'>
+            <span className='number'>3</span>
+            <FontAwesomeIcon size='xl' icon={faEnvelope} />
+          </span>
+          <img src={avatar} alt='avatar' />
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
