439f5e3e6bbb3ed4ef3c83b17b11eb263c67a384
ahmed-m-abdelfatah <ahmedmo.engineer@gmail.com>
2023-01-18 20:34:13 +0200

    ui done sidebar

--- client/src/components/Sidebar/Sidebar.js
+++ client/src/components/Sidebar/Sidebar.js
@@ -1,16 +1,58 @@
 import './Sidebar.scss';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {
+  faHouseChimney,
+  faCalendarDays,
+  faBook,
+  faGraduationCap,
+  faChartLine,
+  faBullhorn,
+} from '@fortawesome/free-solid-svg-icons';
 
 const Sidebar = () => {
   return (
     <aside className='sidebar'>
       <h3>Coligo</h3>
-      <p>
-        close <img src={require('../../img/xmark-solid.svg').default} alt='mySvgImage' />
-      </p>
       <ul>
         <li>
-          <a href='./#'>
+          <a className='active' href='./#'>
+            <FontAwesomeIcon size='xl' icon={faHouseChimney} />
             <span>Dashboard</span>
+          </a>
+        </li>
+
+        <li>
+          <a href='./#'>
+            <FontAwesomeIcon size='xl' icon={faCalendarDays} />
+            <span>Schedule</span>
+          </a>
+        </li>
+
+        <li>
+          <a href='./#'>
+            <FontAwesomeIcon size='xl' icon={faBook} />
+            <span>Courses</span>
+          </a>
+        </li>
+
+        <li>
+          <a href='./#'>
+            <FontAwesomeIcon size='xl' icon={faGraduationCap} />
+            <span>Grade book</span>
+          </a>
+        </li>
+
+        <li>
+          <a href='./#'>
+            <FontAwesomeIcon size='xl' icon={faChartLine} />
+            <span>Performance</span>
+          </a>
+        </li>
+
+        <li>
+          <a href='./#'>
+            <FontAwesomeIcon size='xl' icon={faBullhorn} />
+            <span>Announcement</span>
           </a>
         </li>
       </ul>
